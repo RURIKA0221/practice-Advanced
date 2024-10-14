afd81d15efa7f7ceae283245b04c1684f0c2dffc
Rurika Fuyuki <181914382+RURIKA0221@users.noreply.github.com>
2024-10-14 13:08:48 +0900

    10/13時点

---
+++ src/practice/Chapter1.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Human human = new Human();
+		Human.Leg leg = human.new Leg(); 
+		leg.kick();
+	}
+
+}
+
---
+++ src/practice/Human.java
@@ -0,0 +1,12 @@
+package practice;
+
+public  class Human {
+
+	public class Leg {
+		public void kick() {
+			System.out.println("蹴る");
+		}
+
+	}
+
+}
